92fd96662f7cb298689034860420a8d3ba9eca82
Daria Diachkova <daria.diachkova@mail.ru>
2020-12-19 22:11:36 +0300

    Add day 2 part 2 solution

--- Day 2/Day_2.js
+++ Day 2/Day_2.js
@@ -30,7 +30,6 @@ How many passwords are valid according to their policies?
 import { passwordsList } from './Day_2_input'
 
 let validPasswords = []
-//{from: "15", to: "16", character: "f", password: "ffffffffffffffhf"}
 
 const checkRepeatInPassword = ( password, character ) => {
   return password
@@ -51,3 +50,34 @@ for ( let passwordData of passwordsList ) {
 }
 
 console.log(`There are ${validPasswords.length} valid passwords.`)
+
+/** Part 2
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+
+*/
+
+let validPasswordsByNewPolicy = []
+
+for ( let passwordData of passwordsList ) {
+  let { to, from, password, character } = passwordData
+  let isCharacterAtFirstPosition = password[from - 1] === character
+  let isCharacterAtSecondPosition = password[to - 1] === character
+
+  let isValid = isCharacterAtFirstPosition && !isCharacterAtSecondPosition
+    || !isCharacterAtFirstPosition && isCharacterAtSecondPosition
+    isValid && validPasswordsByNewPolicy.push(password)
+}
+
+console.log(`There are ${validPasswordsByNewPolicy.length} valid passwords.`)
